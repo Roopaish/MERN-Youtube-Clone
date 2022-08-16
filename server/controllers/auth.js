d9ba5cedaae0349ea4a55b72c3dc58c426387dce
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-08-16 15:27:20 +0545

    signup, signin and exception handling

---
+++ server/controllers/auth.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const signup = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+
+    await newUser.save();
+    res.status(200).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return next(createError(404, "Wrong credentials!"));
+
+    const { password, ...others } = user._doc;
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true, // 3rd party wont be able to use our cookie
+      })
+      .status(200)
+      .json(others);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 
 // import routes (name as you want cause they are default exports)
+import authRoutes from "./routes/auth.js";
 import commentsRoutes from "./routes/comments.js";
 import usersRoutes from "./routes/users.js";
 import videosRoutes from "./routes/videos.js";
@@ -22,10 +24,29 @@ const connectDB = () => {
     });
 };
 
+// Take json as req body
+app.use(express.json());
+
+// Enable the use of cookies
+app.use(cookieParser());
+
 // registering routes
+app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/videos", videosRoutes);
 app.use("/api/comments", commentsRoutes);
+
+// middleware to handle exception
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+
+  return res.status(status).json({
+    success: false,
+    status, //eqv. status: status
+    message,
+  });
+});
 
 app.listen(8800, () => {
   connectDB();
